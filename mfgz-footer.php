7db28b7fb7cc468e1bba7fa9f1fa3d7a58fe3f2e
Jane Schindler <jschindler@Janes-MBP.fritz.box>
2018-03-25 14:07:01 +0200

    initial

---
+++ mfgz-footer.php
@@ -0,0 +1,71 @@
+<footer id="colophon" class="site-footer" role="contentinfo">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="site-footer-inner col-sm-12">
+
+        <div class="row row-1">
+          <div class="col-sm-2 footer-logo">
+            Museum <br>für Gestaltung <br>Zürich
+          </div>
+          <div class="col-sm-3">
+            Museum für Gestaltung Zürich<br>
+            Ausstellungsstrasse 60<br>
+            <?php print __('[:de]Postfach[:en]P.O. Box[:fr]Case postale[:]'); ?>, 8031 <?php print __('[:de]Zürich[:en]Zurich[:fr]Zurich[:]'); ?><br><br>
+          </div>
+          <div class="col-sm-3">
+            Museum für Gestaltung Zürich<br>
+            Toni-Areal, Pfingstweidstrasse 96<br>
+            <?php print __('[:de]Postfach[:en]P.O. Box[:fr]Case postale[:]'); ?>, 8031 <?php print __('[:de]Zürich[:en]Zurich[:fr]Zurich[:]'); ?><br><br>
+          </div>
+          <div class="col-sm-2">
+      <div class="social-links">
+        <li class="facebook"><a href="http://www.facebook.com/pages/Zurich-Switzerland/Museum-fur-Gestaltung-Zurich/74825495877?ref=ts" target="_blank">u</a></li>
+        <li class="instagram"><a href="https://instagram.com/museumgestaltung/" target="_blank">y</a></li>
+        <!--<li class="pinterest"><a href="https://www.pinterest.com/source/sammlungen-archive.zhdk.ch/" target="_blank">v</a></li>
+        <li class="gplus"><a href="https://plus.google.com/u/0/+museumf%C3%BCrgestaltungz%C3%BCrich" target="_blank">w</a></li>-->
+        <li class="youtube"><a href="https://www.youtube.com/channel/UCiYgRdZ5GYQr6eFxPyqPXHg" target="_blank"><img src="/wp-content/plugins/add-to-any/icons/youtube.svg" /></a></li>
+        <li class="twitter"><a href="https://twitter.com/Design_Museum" target="_blank">x</a></li>
+      </div>
+          </div>
+          <div class="col-sm-2">
+						<div class="zhdk-img">
+							<a href="http://<?php print __('[:de]zhdk.ch[:en]zhdk.ch/en[:fr]zhdk.ch/en[:]'); ?>" target="_blank" ><img src="<?php print get_template_directory_uri(); ?>/includes/2018_zhdk_logo_<?php print __('[:de]D[:en]E[:fr]E[:]'); ?>_weiss.svg" /></a>
+						</div>
+          </div>
+        </div>
+
+        <div class="row row-2">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-3">
+						<a href="<?php print get_permalink(4511); ?>"><?php print __('[:de]Kontakt[:en]Contact[:fr]Contact[:]'); ?></a><br>
+						<a href="<?php print get_permalink(4273); ?>"><?php print __('[:de]Presse[:en]Press[:fr]Presse[:]'); ?></a>
+          </div>
+          <div class="col-sm-3">
+            <?php $discl_link = get_permalink(4601); ?>
+						<a href="<?php print $discl_link; ?>"><?php print __('[:de]Disclaimer / Impressum[:en]Disclaimer / Credits[:fr]Mentions légales / Crédits[:]'); ?></a><br>
+          </div>
+          <div class="col-sm-2">
+      <div class="social-links">
+        <li class="facebook"><a href="http://www.facebook.com/pages/Zurich-Switzerland/Museum-fur-Gestaltung-Zurich/74825495877?ref=ts" target="_blank">u</a></li>
+        <li class="instagram"><a href="https://instagram.com/museumgestaltung/" target="_blank">y</a></li>
+        <!--<li class="pinterest"><a href="https://www.pinterest.com/source/sammlungen-archive.zhdk.ch/" target="_blank">v</a></li>
+        <li class="gplus"><a href="https://plus.google.com/u/0/+museumf%C3%BCrgestaltungz%C3%BCrich" target="_blank">w</a></li>-->
+        <li class="youtube"><a href="https://www.youtube.com/channel/UCiYgRdZ5GYQr6eFxPyqPXHg" target="_blank"><img src="/wp-content/plugins/add-to-any/icons/youtube.svg" /></a></li>
+        <li class="twitter"><a href="https://twitter.com/Design_Museum" target="_blank">x</a></li>
+      </div>
+          </div>
+          <div class="col-sm-2">
+						<div class="zhdk-img">
+							<a href="http://<?php print __('[:de]zhdk.ch[:en]zhdk.ch/en[:fr]zhdk.ch/en[:]'); ?>" target="_blank" ><img src="<?php print get_template_directory_uri(); ?>/includes/2018_zhdk_logo_<?php print __('[:de]D[:en]E[:fr]E[:]'); ?>_weiss.svg" /></a>
+						</div>
+          </div>
+        </div>
+
+      </div>
+
+			</div>
+		</div>
+	</div><!-- close .container -->
+</footer><!-- close #colophon -->
