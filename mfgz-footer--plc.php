f6c97bd77f6271cf9cc500252186d7c12abaeb9d
Jane <schindler@stillhartkonzept.ch>
2019-01-31 13:41:10 +0100

    fix(footer): some changes for plc footer

--- mfgz-footer--plc.php
+++ mfgz-footer--plc.php
@@ -5,13 +5,13 @@
               <div class="row">
                   <div class="col-sm-4 footer__links">
                       <div class="footer__link">
-                          <a href="https://museum-gestaltung.ch/<?php print __('[:de]de[:en]en[:fr]fr'); ?>/kontakt/"><?php print __('[:de]Kontakt[:en]Contact[:fr]Contact[:]'); ?></a>
+                          <a href="<?php print __('[:de]de[:en]en[:fr]fr'); ?>/ueber-uns"><?php print __('[:de]Kontakt[:en]Contact[:fr]Contact[:]'); ?></a>
                       </div>
                       <div class="footer__link">
-                        <a href="https://museum-gestaltung.ch/<?php print __('[:de]de[:en]en[:fr]fr'); ?>/pressebereich/"><?php print __('[:de]Medien[:en]Media[:fr]Média[:]'); ?></a>
+                        <a href="https://museum-gestaltung.ch/<?php print __('[:de]de[:en]en[:fr]fr'); ?>/pressebereich" target="_blank"><?php print __('[:de]Medien[:en]Media[:fr]Média[:]'); ?></a>
                       </div>
                       <div class="footer__link">
-                        <?php $discl_link = 'https://museum-gestaltung.ch/'.__('[:de]de[:en]en[:fr]fr[:]').'/disclaimer/'; ?>
+                        <?php $discl_link = '/'.__('[:de]de[:en]en[:fr]fr[:]').'/rechtliche-hinweise'; ?>
                         <a href="<?php print $discl_link; ?>"><?php print __('[:de]Rechtliches / Impressum[:en]Legal information / Credits[:fr]Mentions légales / Crédits[:]'); ?></a>
                       </div>
                       <!--<div class="footer__link">
@@ -29,7 +29,7 @@
                         <?php print __('[:de]Ein Angebot von:[:en]An offer of: [:fr]Une offre de:[:]'); ?><br />
                         <a href="https://www.stadt-zuerich.ch/kultur/de/index.html" target="_blank">Kultur Stadt Zürich</a><br /><br />
                         <?php print __('[:de]Inhalt und Betrieb:[:en]Content and operation:[:fr]Contenu et administration:[:]'); ?><br />
-                        <a href=" https://museum-gestaltung.ch/de" target="_blank">Museum für Gestaltung Zürich</a>
+                        <a href=" https://museum-gestaltung.ch/<?php print __('[:de]de[:en]en[:fr]fr'); ?>" target="_blank">Museum für Gestaltung Zürich</a>
                     </div>
                   </div>
               </div>
