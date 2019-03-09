4f88c36f33c35b27e51ad1a1eb3bc70e1337f0a4
LuxsThor <lucas.thorbecke@bluewin.ch>
2019-03-09 12:43:56 +0100

    added datenschutz plut some samll text changes

--- mfgz-footer--plc.php
+++ mfgz-footer--plc.php
@@ -14,9 +14,10 @@
                         <?php $discl_link = '/'.__('[:de]de[:en]en[:fr]fr[:]').'/rechtliche-hinweise'; ?>
                         <a href="<?php print $discl_link; ?>"><?php print __('[:de]Rechtliches / Impressum[:en]Legal information / Credits[:fr]Mentions légales / Crédits[:]'); ?></a>
                       </div>
-                      <!--<div class="footer__link">
-                        TODO Datenschutz!
-                      </div>-->
+                      <div class="footer__link">
+                        <?php $discl_link = '/'.__('[:de]de[:en]en[:fr]fr[:]').'/rechtliche-hinweise'; ?>
+                        <a href="<?php print $discl_link; ?>"><?php print __('[:de]Datenschutz[:en]Privacy[:fr]Confidentialité[:]'); ?></a>
+                      </div>
                   </div>
                   <div class="col-sm-4 footer__links">
                     <ul class="social-links">
@@ -28,7 +29,7 @@
                     <div class="footer__organisation">
                         <?php print __('[:de]Ein Angebot von:[:en]An offer of:[:fr]Une offre de:[:]'); ?><br />
                         <a href="https://www.stadt-zuerich.ch/kultur/de/index.html" target="_blank">Kultur Stadt Zürich</a><br /><br />
-                        <?php print __('[:de]Inhalt und Betrieb:[:en]Content and operation:[:fr]Contenu et administration:[:]'); ?><br />
+                        <?php print __('[:de]Leitung und Kuratorium:[:en]Direction and curation:[:fr]Direction et commissariat:[:]'); ?><br />
                         <a href=" https://museum-gestaltung.ch/<?php print __('[:de]de[:en]en[:fr]fr'); ?>" target="_blank">Museum für Gestaltung Zürich</a>
                     </div>
                   </div>
